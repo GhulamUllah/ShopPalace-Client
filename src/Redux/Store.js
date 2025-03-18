db86a0953a0d2975c3a9c33bc87074b41baf140f
Ghulam Ullah <ghulamullahkhan84@gmail.com>
2025-03-18 16:11:51 +0500

    add

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -3,7 +3,6 @@ import thunk from "redux-thunk";
 import rootReducer from "./Reducer/combineReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 const middleware = [thunk];
-const production = process.env.NODE_ENV === "production";
 
-const Store = createStore(rootReducer, composeWithDevTools(production ? middleware : applyMiddleware(...middleware)));
+const Store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)));
 export default Store;
